893a2dfb83f0b8bee11923d9cec5dc7e180039ca
Tim Seed <tim@sy-edm.com>
2019-06-30 10:56:45 +0800

    K3 type added

--- ham/radio/k3.py
+++ ham/radio/k3.py
@@ -5,13 +5,12 @@
 # Licensed under Academic Free License 3.0 (See LICENSE)
 
 
-'''
+"""
 Not all the functions in this modules have been tested - and not all appear to work as expected.
 
 Further works remains here
 
-'''
-
+"""
 
 import logging
 import serial
@@ -33,47 +32,48 @@ class K3:
         self.ser.flushInput()  # until the "quit" comes along.
         self.ser.close()
 
-    def write(self, str):
-        for a in str.split(';'):
+    def write(self, str_command: str) -> None:
+        for a in str_command.split(';'):
             self.logger.debug("Sending Data to Serial Port")
             self.ser.write((a + ';').encode('utf-8'))
             # self.ser.flushInput()
 
-    def read(self, n):
+    def read(self, bytes_to_read: int) -> str:
         try:
-            return self.ser.read(n).decode('utf-8')
+            return self.ser.read(bytes_to_read).decode('utf-8')
         except:
             self.logger.error("Error Reading data")
             return ''
+
     #
     # Set the VFO current frequency
     # FA00014060000
     # FA00007030000
-    def qsy(self, freq):
-        if (freq < 10e6):
-            cmd = "FA0000%d;" % (freq)  # in 7 digits
+    def qsy(self, freq: float) -> str:
+        if freq < 10e6:
+            cmd = "FA0000%d;" % freq  # in 7 digits
         else:
-            cmd = "FA000%d;" % (freq)  # in 8 digits
+            cmd = "FA000%d;" % freq  # in 8 digits
         self.write(cmd)
-        return (self.qsyq())
+        return self.qsyq()
 
     #
     # Set the 2ND RX VFO current frequency
     # FB00014060000
     # FB00007030000
-    def qsy2(self, freq):
-        if (freq < 10e6):
-            cmd = "FB0000%d;" % (freq)  # in 7 digits
+    def qsy2(self, freq: float) -> str:
+        if freq < 10e6:
+            cmd = "FB0000%d;" % freq  # in 7 digits
         else:
-            cmd = "FB000%d;" % (freq)  # in 8 digits
+            cmd = "FB000%d;" % freq  # in 8 digits
         self.write(cmd)
-        return (str(self.qsyq()))
+        return str(self.qsyq())
 
     #
     # Get the VFO current frequency
     # FA00014060000
     # FA00007030000
-    def qsyq(self):
+    def qsyq(self) -> str:
         # self.ser.flushOutput()
         # self.ser.flushInput()
         self.logger.debug("get VFO-A")
@@ -91,7 +91,7 @@ class K3:
     # Get the 2ND RX VFO current frequency
     # FA00014060000
     # FA00007030000
-    def qsyq2(self):
+    def qsyq2(self) -> str:
         self.logger.debug("Get VFO-B")
         self.write("FB;")
         result = self.read(20)
@@ -107,29 +107,29 @@ class K3:
     # Get the First IF current frequency
     # FI;
     # FI6500;
-    def fiq(self):
+    def fiq(self) -> str:
         self.write("FI;")
         result = self.read(7)
-        if (len(result) != 7):
-            return 0
+        if len(result) != 7:
+            return "0"
         return "821" + result[2:3] + "." + result[3:6]
 
     #
     # Set NB to level (off=0, 1, 2)
     # TODO: use SW22; to change thresh but we have to ready it first
     def nb(self, level, thresh):
-        self.write("NB%s;" % (level))
+        self.write("NB%s;" % level)
 
     #
     # Get the NB setting
     # NB
     # NB00; Off, hi thresh
     # NB21; NB2, low thres
-    def nbq(self):
+    def nbq(self) -> str:
         self.write("NB;")
         result = self.read(5)
-        if (len(result) != 5):
-            return 0
+        if len(result) != 5:
+            return "0"
         level = result[2]
         thresh = result[3]
         return "NB: %c %c" % (level, thresh)
@@ -138,72 +138,77 @@ class K3:
     # Set the Mode
     # MD1; LSB
     # FA00007030000
-    def mode(self, str):
+    def mode(self, str_mode_setting: str) -> str:
         """
         Set the Mode.
-        :param str: Input values are lsb usb cw cwr rtty
+        :param str_mode_setting: Input values are lsb usb cw cwr rtty
         :return: Output from method modeq
         """
-        str = str.lower()
-        if str == "lsb" or str == "l":
-            modenum = 1
-        if str == "usb" or str == "u":
-            modenum = 2
-        if str == "cw" or str == "c":
-            modenum = 3
-        if str == "cwr":
-            modenum = 5
-        if str == "rtty" or str == "r":
-            modenum = 6
-        if str == "rttyr":
-            modenum = 9
-        cmd = "MD%d;" % (modenum)
+        str_mode_setting = str_mode_setting.lower()
+        mode_num: int = 0
+        if str_mode_setting == "lsb" or str_mode_setting == "l":
+            mode_num = 1
+        elif str_mode_setting == "usb" or str_mode_setting == "u":
+            mode_num = 2
+        elif str_mode_setting == "cw" or str_mode_setting == "c":
+            mode_num = 3
+        elif str_mode_setting == "cwr":
+            mode_num = 5
+        elif str_mode_setting == "rtty" or str_mode_setting == "r":
+            mode_num = 6
+        elif str_mode_setting == "rttyr":
+            mode_num = 9
+        cmd = "MD%d;" % mode_num
         self.write(cmd)
         return self.modeq()
 
     #
     # Get the mode
-    def modeq(self):
+    def modeq(self) -> str:
         self.write("K22;MD;")
         result = self.read(4)
-        if (len(result) != 4):
+        if len(result) != 4:
             return "???: " + result
-        modenum = int(result[2])
+        try:
+            modenum = int(result[2])
+        except ValueError:
+            return "unk"
         if modenum == 1:
             return "lsb"
-        if modenum == 2:
+        elif modenum == 2:
             return "usb"
-        if modenum == 3:
+        elif modenum == 3:
             return "cw"
-        if modenum == 5:
+        elif modenum == 5:
             return "cwr"
-        if modenum == 6:
+        elif modenum == 6:
             return "rtty"
-        if modenum == 9:
+        elif modenum == 9:
             return "rttyr"
-        return "???"
+        else:
+            return "???"
 
     #
     # Set the preamp
     # PA0; off
     # PA1; on
-    def pa(self, str='on'):
-
-        if (str.lower() == "off"):
+    def pa(self, pre_amp_setting='on') -> str:
+        modenum = ""
+        if pre_amp_setting.lower() == "off":
             modenum = 0
-        if (str.lower() == "on"):
+        if pre_amp_setting.lower() == "on":
             modenum = 1
-        cmd = "PA%d;" % (modenum)
+        cmd = "PA%d;" % modenum
         self.write(cmd)
         return self.paq()
 
     #
     # Get the preamp
-    def paq(self):
+    def paq(self) -> str:
         self.write("PA;")
         result = self.read(4)
-        if (len(result) != 4):
-            return 0
+        if len(result) != 4:
+            return "0"
         if result == "PA1;":
             return "on"
         else:
@@ -213,29 +218,32 @@ class K3:
     # Set the power output
     # PC000; off
     # PC010; 10 watts
-    def power(self, watts):
-        cmd = "PC%03d;" % (watts)
+    def power(self, watts: int) -> str:
+        cmd = "PC%03d;" % watts
         self.write(cmd)
         return self.powerq()
 
     #
     # Get the power output
-    def powerq(self):
-        self.write("PC;");
-        result = self.read(6);
-        if (len(result) != 6):
-            return 0
-        return str(int(result[2:5]) / 10)
+    def powerq(self) -> str:
+        self.write("PC;")
+        result = self.read(6)
+        if len(result) != 6:
+            return "0"
+        try:
+            return str(int(result[1:4]) / 10)
+        except ValueError:
+            return "??"
 
     #
     # Set to VFO A for TX and RX
-    def vfoa(self):
+    def vfoa(self) -> str:
         self.write("FT0;FR0;")
         return self.vfoq()
 
     #
     # Get the B for TX and RX
-    def vfob(self):
+    def vfob(self) -> str:
         self.write("FT1;FR1;")
         return self.vfoq()
 
@@ -243,20 +251,20 @@ class K3:
     # Get the VFO current frequency
     # FA00014060000
     # FA00007030000
-    def vfoq(self):
+    def vfoq(self) -> str:
         self.write("FT;FR;")
-        result = self.read(8);
-        if (len(result) != 8):
-            return 0
+        result = self.read(8)
+        if len(result) != 8:
+            return "0"
         #    return "TX: %c; RX: %c" %(65+int(result[2]), 65+int(result[6]))
         return "Unk"
 
     #
     # Send CW
     # KY text;
-    def sendcw_orig(self, str):
+    def sendcw_orig(self, cw_text: str) -> None:
         try:
-            for a in str.split(';'):
+            for a in cw_text.split(';'):
                 cmd = a + ';'
                 self.logger.debug("Sending " + cmd)
                 self.write(cmd)
@@ -264,33 +272,33 @@ class K3:
         except:
             logging.warning("sendcw issue")
 
-    def sendcw(self, str):
+    def sendcw(self, cw_text: str) -> None:
         try:
-            self.logger.debug("Sending <" + str + ">")
-            self.write(str)
+            self.logger.debug("Sending <" + cw_text + ">")
+            self.write(cw_text)
             self.logger.debug("CW Sent")
         except:
             logging.warning("sendcw issue")
 
     # TODO
     # Set speed 15=K015;
-    def cwspeedq(self):
+    def cwspeedq(self) -> str:
         cmd = "KS;"
         self.write(cmd)
-        result = self.read(5);
-        if (len(result) != 5):
-            return 0
+        result = self.read(5)
+        if len(result) != 5:
+            return "0"
         return result[1:3]
 
     # TODO
-    def cwspeed(self, speed):
+    def cwspeed(self, speed) -> str:
         cmd = "K%3d;"
         self.write(cmd)
         return self.cwspeedq()
 
     #
     # Set ATT to off=0|on=1
-    def ra(self, offon):
+    def ra(self, offon) -> str:
         return "Unused"
 
     #
@@ -307,7 +315,7 @@ class K3:
     # FW00001;
     # 0 means "next"
     def filter(self, n):
-        cmd = "FW0000%d;" % (n)
+        cmd = "FW0000%d;" % n
         self.write(cmd)
 
     #
@@ -315,7 +323,7 @@ class K3:
     def filtern(self):
         self.write("FW;")
         result = self.read(9)
-        if (len(result) != 9):
+        if len(result) != 9:
             return str(0)
         return str(int(result[6]))
 
@@ -324,7 +332,7 @@ class K3:
     def filterq(self):
         self.write("FW;")
         result = self.read(9)
-        if (len(result) != 9):
+        if len(result) != 9:
             return 0
         return result[2:6] + "Hz " + result[7]
 
@@ -342,31 +350,31 @@ class K3:
     # RVF;
     # -> RV*nn.nn;
     def verq(self, x):
-        self.write("RV%c;" % (x))
+        self.write("RV%c;" % x)
         result = self.read(9)
-        if (len(result) != 9):
+        if len(result) != 9:
             return 0
         ver = result[3:8]
         if ver == '99.99':
             ver = None
         return ver
 
-    def showtext(self, text):
+    def showtext(self, text: str) -> None:
         for letter in text:
-            self.write("DB%s;" % (letter))
+            self.write("DB%s;" % letter)
 
-    def k3LcdChar(c):
+    def k3lcdchar(self, c) -> str:
         if ord(c) > 127:
-            return ":" + chr(ord(c) & 127)
+            return ":" + str(chr(ord(c) & 127))
         else:
-            return c
+            return str(c)
 
-    def timeq(self):
+    def timeq(self) -> str:
         return "Unused"
 
-    def fixtime(self):
+    def fixtime(self) -> None:
         def k3LcdChar(c):
-            return chr(ord(c) & 127)
+            return str(chr(ord(c) & 127))
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='ham',
-    version='1.0.0.11',
-    packages=['ham', 'ham.rbn', 'ham.rbn.build', 'ham.rbn.build.lib', 'ham.adif', 'ham.dxcc', 'ham.beacon',
-              'ham.qsosvr', 'ham.telnet'],
+    version='1.0.2',
+    packages=['Evt', 'Gui', 'ham', 'ham.rbn', 'ham.adif', 'ham.band', 'ham.calc', 'ham.dxcc', 'ham.radio', 'ham.beacon',
+              'ham.kpa500', 'ham.qsosvr', 'ham.telnet', 'ham.rotator', 'dxcc', 'map2', 'txtlib'],
     url='',
     license='',
     author='tim',
-    author_email='tim@sy-edm.com',
-    description='',
-    requires=["PyQt5"          "wxPython==4.0.6", "cheroot", "CherryPy", "portend", "PyAudio", "pyserial", "pytz",
-              "sip", "six", "tempora"]
+    author_email='',
+    description=''
 )
