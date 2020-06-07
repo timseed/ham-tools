6655cf77f084aa6f9a0154f517f1098218337b43
Tim Seed <tim@sy-edm.com>
2020-06-07 16:06:26 +0800

    Bump version: 1.8.0 → 1.8.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ for p in find_packages():
     print("Instaling ipackage "+str(p))
 setup(
     name='ham',
-    version='1.8.0',
+    version='1.8.1',
     packages=find_packages(),
     include_package_data=True, #Uses Manifest.IN
     url='',
@@ -27,7 +27,7 @@ setup(
         ]},
     #Dev can be triggered by
     #python setup.py sdist
-    #pip install dist/ham-1.8.0.tar.gz[dev]
+    #pip install dist/ham-1.8.1.tar.gz[dev]
     #
 )
 
